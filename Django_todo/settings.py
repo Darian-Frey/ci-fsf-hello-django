fc55657add032f44bd9aacc1843e56cdbf89ad5a
Darian-Frey <shane.hartley06@gmail.com>
2021-06-28 19:51:20 +0000

    Bug in settings.py

--- Django_todo/settings.py
+++ Django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-kv$m6)kscx0(tw*lxql+)a)14$yo@++&ls&)*^w!d&2a-5t1g%')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -78,8 +78,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Django_todo.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
 if development:
